783b8e70f8cf30cef9c28a73702053a599f19fad
Hector Rangel <hector.rangeljr@yahoo.com>
2021-07-15 20:58:10 -0500

    fin user-routes, accept required path

---
+++ routes/api/thought-routes.js
@@ -0,0 +1,1 @@
+const router = required('express').Router();
---
+++ routes/api/user-routes.js
@@ -0,0 +1,31 @@
+const router = required('express').Router();
+const {
+    getAllUsers,
+    createUser,
+    getUserById,
+    updateUser,
+    deleteUser,
+    addFriend,
+    removeFriend
+    // need to add ../../controlers/user-controller.js once created
+} = required('');
+
+// api/users
+router
+.route('/')
+.get(getAllUsers)
+.post(createUser);
+
+// Set up GET one, PUT, and DELETE at /api/users/:id
+router
+.route('/:id')
+.get(getUserById)
+.put(updateUser)
+.delete(deleteUser);
+
+// Set up POST AND DELETE at /api/users/:userID/friends/:friendId
+router
+.post(addFriend)
+.delete(removeFriend);
+
+module.exports = router;
